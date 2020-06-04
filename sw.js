24620ea9eb6f0e1abfcf617c61f7f89e78ba0c1b
huhuantao <huhuantao@ruijie.com.cn>
2020-06-04 18:07:07 +0800

    Site updated: 2020-06-04 18:06:48

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","8f8630324850aae9b89ad303cc74956b"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
